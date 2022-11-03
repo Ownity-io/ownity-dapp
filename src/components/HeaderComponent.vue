b814426c55ed775fafa9903d192b913f67905efd
Elena Pohrebniak <68587473+ElenaPohrebniak@users.noreply.github.com>
2022-11-03 16:43:59 +0200

    added fonts, styles, finished basic header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,42 @@
 <template>
-    <div>
-        Header
-    </div>
+    <header>
+        <div class="container">
+            <div class="header-wrapper">
+                <div class="header-logo">Marketplace</div>
+                <div class="header-search">
+                    <div class="input-wrapper search-wrapper">
+                        <i class="i-search-line"></i>
+                        <input type="text" placeholder="Search NFT, collections, id">
+                    </div>
+                </div>
+                <div class="header-nav">
+                    <nav>
+                        <ul class="">
+                            <li><a :class="{active: headerItemActive1}" href="">Marketplace</a></li>
+                            <li><a :class="{active: headerItemActive2}" href="">Collections</a></li>
+                            <li><a :class="{active: headerItemActive3}" href="">Help</a></li>
+                        </ul>
+                    </nav>
+                    <div class="btn-container">
+                        <button class="btn btn-connect">
+                            Connect Wallet
+                        </button>
+                    </div>
+                </div>
+                <div class="header-mobile"></div>
+            </div>
+        </div>
+    </header>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            headerItemActive1:true,
+            headerItemActive2:false,
+            headerItemActive3:false,
+        }
+    }
+}
+</script>
--- src/views/Marketplace.vue
+++ src/views/Marketplace.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
+    <div class="page-wrapper">
         Hello Marketplace
+        <main>
+            <div class="container">
+                <div class="breadcrumbs">
+                    <!-- <li><router-link :to='{ name: Main}'>Home</router-link></li> -->
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Marketplace</a></li>
+                </div>
+            </div>
+        </main>
     </div>
 </template>
