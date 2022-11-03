0767dfa8ba034100677590940e9a3fae8a3834ce
Elena Pohrebniak <68587473+ElenaPohrebniak@users.noreply.github.com>
2022-11-03 18:01:15 +0200

    added footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,46 @@
 <template>
     <footer>
-        Footer
+        <div class="footer-row footer-row-nav">
+            <div class="container">
+                <div class="footer-section">
+                    <div class="footer-block footer-block-logo">
+                        <div class="footer-logo">
+                            Marketplace
+                        </div>
+                    </div>
+                    <div class="footer-block">
+                        <div class="footer-block-name">
+                            Marketplace
+                        </div>
+                        <ul class="footer-list">
+                            <li><a href="">Active</a></li>
+                            <li><a href="">Canceled</a></li>
+                            <li><a href="">Purchased</a></li>
+                            <li><a href="">On sale</a></li>
+                            <li><a href="">Sold</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-block">
+                        <div class="footer-block-name">
+                            Marketplace
+                        </div>
+                        <ul class="footer-list">
+                            <li><a href="">Active</a></li>
+                            <li><a href="">Canceled</a></li>
+                            <li><a href="">Purchased</a></li>
+                            <li><a href="">On sale</a></li>
+                            <li><a href="">Sold</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-row footer-row-nav">
+            <div class="container">
+                <div class="footer-section">
+
+                </div>
+            </div>
+        </div>
     </footer>
 </template>
