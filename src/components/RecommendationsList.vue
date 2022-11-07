68745a1c132b6d465e7913b748f52ddac6387b22
Elena Pohrebniak <68587473+ElenaPohrebniak@users.noreply.github.com>
2022-11-07 17:16:53 +0200

    start layout for listing page

---
+++ src/components/RecommendationsList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="recommendations-wrap">
+        <div class="header-title-btn">
+          <div class="title">Your Recommendations</div>
+          <div class="btn-wrap">
+            <div class="btn-router-to">
+              To the Collections
+              <i class="i-arrow-right-s-line"></i>
+            </div>
+          </div>
+        </div>
+        <ul class="recommendations-list">
+          <li>
+            <Card/>
+          </li>
+          <li>
+            <Card/>
+          </li>
+          <li>
+            <Card/>
+          </li>
+          <li>
+            <Card/>
+          </li>
+          <li>
+            <Card/>
+          </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Card,
+  },
+};
+</script>
