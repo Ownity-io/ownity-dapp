4a0c97a1fdb3d161069477f84bc2fc8a66a418c2
Denys Osadchuk <beatvin128@gmail.com>
2022-11-08 14:11:31 +0200

    changed filter component to display data from backend

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -6,47 +6,11 @@
         <i class="i-arrow-up-s-line"></i>
       </button>
       <ul class="filter-ul">
-        <li class="filter-li">
+        <li class="filter-li" v-for="item in this.$store.getters['api/getStatuses']" :key="item">
           <div class="input-checkbox">
-            <input type="checkbox" id="test-checkbox1" />
-            <label for="test-checkbox1">
-              <span>Canceled</span>
-              <i class="i-check-line"></i>
-            </label>
-          </div>
-        </li>
-        <li class="filter-li">
-          <div class="input-checkbox">
-            <input type="checkbox" id="test-checkbox2" />
-            <label for="test-checkbox2">
-              <span>Active</span>
-              <i class="i-check-line"></i>
-            </label>
-          </div>
-        </li>
-        <li class="filter-li">
-          <div class="input-checkbox">
-            <input type="checkbox" id="test-checkbox3" />
-            <label for="test-checkbox3">
-              <span>Purchased</span>
-              <i class="i-check-line"></i>
-            </label>
-          </div>
-        </li>
-        <li class="filter-li">
-          <div class="input-checkbox">
-            <input type="checkbox" id="test-checkbox4" />
-            <label for="test-checkbox4">
-              <span>On sale</span>
-              <i class="i-check-line"></i>
-            </label>
-          </div>
-        </li>
-        <li class="filter-li">
-          <div class="input-checkbox">
-            <input type="checkbox" id="test-checkbox5" />
-            <label for="test-checkbox5">
-              <span>Sold</span>
+            <input type="checkbox" :id="item" />
+            <label :for="item">
+              <span>{{item}}</span>
               <i class="i-check-line"></i>
             </label>
           </div>
@@ -59,22 +23,12 @@
         <i class="i-arrow-up-s-line"></i>
       </button>
       <ul class="filter-ul">
-        <li class="filter-li">
+        <li class="filter-li" v-for="item in this.$store.getters['api/getNftCollections']" :key="item">
           <div class="input-checkbox">
-            <input type="checkbox" id="test-checkbox11" />
-            <label for="test-checkbox11">
+            <input type="checkbox" :id="item.contract_address" />
+            <label :for="item.contract_address">
               <div class="icon-filter-checkbox"></div>
-              <span>Mutant Ape Yacht Club</span>
-              <i class="i-check-line"></i>
-            </label>
-          </div>
-        </li>
-        <li class="filter-li">
-          <div class="input-checkbox">
-            <input type="checkbox" id="test-checkbox12" />
-            <label for="test-checkbox12">
-              <div class="icon-filter-checkbox"></div>
-              <span>Mutant Ape Yacht Club</span>
+              <span>{{item.name}}</span>
               <i class="i-check-line"></i>
             </label>
           </div>
@@ -87,32 +41,12 @@
         <i class="i-arrow-up-s-line"></i>
       </button>
       <ul class="filter-ul">
-        <li class="filter-li">
+        <li class="filter-li" v-for="item in this.$store.getters['api/getMarketplaces']" :key="item">
           <div class="input-checkbox">
-            <input type="checkbox" id="test-checkbox21" />
-            <label for="test-checkbox21">
+            <input type="checkbox" :id="item.id" />
+            <label :for="item.id">
               <div class="icon-filter-checkbox"></div>
-              <span>OpenSea</span>
-              <i class="i-check-line"></i>
-            </label>
-          </div>
-        </li>
-        <li class="filter-li">
-          <div class="input-checkbox">
-            <input type="checkbox" id="test-checkbox22" />
-            <label for="test-checkbox22">
-              <div class="icon-filter-checkbox"></div>
-              <span>LooksRare</span>
-              <i class="i-check-line"></i>
-            </label>
-          </div>
-        </li>
-        <li class="filter-li">
-          <div class="input-checkbox">
-            <input type="checkbox" id="test-checkbox23" />
-            <label for="test-checkbox23">
-              <div class="icon-filter-checkbox"></div>
-              <span>X2Y2</span>
+              <span>{{item.name}}</span>
               <i class="i-check-line"></i>
             </label>
           </div>
