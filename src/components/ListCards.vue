0bb9d8b98039bdbd86743fa76d89d5669a443f2d
Denys Osadchuk <beatvin128@gmail.com>
2022-11-08 15:36:58 +0200

    fixed empty csrd list div on the bottom

--- src/components/ListCards.vue
+++ src/components/ListCards.vue
@@ -3,12 +3,12 @@
     <div v-if="this.$store.getters['api/getListingsResults'].length>0" class="cards-list-container">
       <Card v-for="item in this.$store.getters['api/getListingsResults']" :key="item" :item="item" />
     </div>
-    <div class="cards-list-load" ref="target" v-if="this.$store.getters['api/getLastListingsResponse']!=null">
+    <div class="cards-list-load" ref="target" v-if="this.$store.getters['api/getLastListingsResponse']!=null & this.$store.getters['api/getListingsResults'].length>0">
         <div class="i-wrap">
             <i class="i-loader-4-line"></i>
         </div>
     </div>
-    <div class="cards-list-empty" v-else>
+    <div class="cards-list-empty" v-if="this.$store.getters['api/getListingsResults'].length==0">
       <div class="title">Oops! Nothing here. </div>
       <button class="btn">
         Back to all items
