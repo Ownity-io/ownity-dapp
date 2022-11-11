6d43adf1d85a148392841b3e5b05ba25b5c8250f
beatvin <beatvin128@gmail.com>
2022-11-11 14:43:07 +0200

    auth finished

--- src/store/modules/walletsAndProvider.js
+++ src/store/modules/walletsAndProvider.js
@@ -1,11 +1,15 @@
 import { ethers } from "ethers";
 import WalletConnectProvider from "@walletconnect/web3-provider";
+import config from "@/config.json";
 export default {
   namespaced: true,
   state() {
     return {
       globalProvider: null,
       token: null,
+      tokenEndTimestamp: null,
+      refreshToken:null,
+      nonce:null,
       address:null,
       shortAddress:null
     };
@@ -38,16 +42,85 @@ export default {
       try {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         await provider.send("eth_requestAccounts", []);        
-        context.commit('setGlobalProvider',provider);
-        await provider.send(
-          'wallet_switchEthereumChain',
-          [{ chainId: "0x1"}],
-        );
+        context.commit('setGlobalProvider',provider);        
+        let userAddress = await provider.getSigner().getAddress();
+        if (localStorage.getItem('userAddress')!=userAddress){
+          localStorage.setItem('userAddress',null);
+          localStorage.setItem('token',null);
+          localStorage.setItem('tokenEndTimestamp',null);
+          localStorage.setItem('refreshToken',null);
+          localStorage.setItem('nonce',null);
+        }
+        if (localStorage.getItem('userAddress')==null || localStorage.getItem('userAddress')=='null'){
+          //getNonce
+          let requestUrl = `${config.backendApiEntryPoint}registration/`;
+          let request = await fetch(
+            requestUrl,
+            {
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              method: "POST",
+              body: JSON.stringify({address: userAddress})
+            }
+          );
+          let nonce = (await request.json()).data.nonce;
+          console.log(nonce);
+          //SignMessage
+          let msg = `${config.messageToSignStart}${nonce}`;
+          let signedMessage = await provider.getSigner().signMessage(msg);
+          console.log(signedMessage);
+          //login
+          requestUrl = `${config.backendApiEntryPoint}login/`;
+          request = await fetch(
+            requestUrl,
+            {
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              method: "POST",
+              body: JSON.stringify({address: userAddress,signed_message:signedMessage})
+            }
+          );
+          let requestJson = await request.json();
+          console.log(requestJson.data.refresh);
+          localStorage.setItem('token', requestJson.data.token);
+          localStorage.setItem('tokenEndTimestamp', requestJson.data.expiration_timestamp);
+          localStorage.setItem('refreshToken', requestJson.data.refresh);
+        }else if (Date.parse(localStorage.getItem('tokenEndTimestamp'))<Date.now()){
+          //gettoken with refesh-token
+          let requestUrl = `${config.backendApiEntryPoint}token-refresh/`;
+          let request = await fetch(
+            requestUrl,
+            {
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              method: "POST",
+              body: JSON.stringify({refresh: localStorage.getItem('refreshToken')})
+            }
+          );
+          requestJson = await request.json();
+          //write info to localStorage
+          localStorage.setItem('token', requestJson.access);
+          localStorage.setItem('tokenEndTimestamp', (Date.now()/1000)+86400);
+          localStorage.setItem('refreshToken', requestJson.refresh);
+        }
+        await provider.send('wallet_switchEthereumChain',[{ chainId: "0x1"}]);
         context.commit('setUserInfo',await provider.getSigner().getAddress());
         localStorage.setItem('connectedWallet','metamask');
         localStorage.setItem('userAddress',await provider.getSigner().getAddress());
-      } catch {
+      } catch (error){
+        console.log(error);
         localStorage.setItem('connectedWallet',null);
+        localStorage.setItem('userAddress',null);
+        localStorage.setItem('token',null);
+        localStorage.setItem('tokenEndTimestamp', null);
+        localStorage.setItem('refreshToken', null);
+        localStorage.setItem('nonce', null);
         console.log('Metamask Connection Error');
       }
     },
@@ -57,6 +130,72 @@ export default {
         await provider.enable()
         const web3Provider = new ethers.providers.Web3Provider(provider);
         context.commit('setGlobalProvider',web3Provider);
+        let userAddress = await web3Provider.getSigner().getAddress();
+        if (localStorage.getItem('userAddress')!=userAddress){
+          localStorage.setItem('userAddress',null);
+          localStorage.setItem('token',null);
+          localStorage.setItem('tokenEndTimestamp',null);
+          localStorage.setItem('refreshToken',null);
+          localStorage.setItem('nonce',null);
+        }
+        if (localStorage.getItem('userAddress')==null || localStorage.getItem('userAddress')=='null'){
+          //getNonce
+          let requestUrl = `${config.backendApiEntryPoint}registration/`;
+          let request = await fetch(
+            requestUrl,
+            {
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              method: "POST",
+              body: JSON.stringify({address: userAddress})
+            }
+          );
+          let nonce = (await request.json()).data.nonce;
+          console.log(nonce);
+          //SignMessage
+          let msg = `${config.messageToSignStart}${nonce}`;
+          let signedMessage = await web3Provider.getSigner().signMessage(msg);
+          console.log(signedMessage);
+          //login
+          requestUrl = `${config.backendApiEntryPoint}login/`;
+          request = await fetch(
+            requestUrl,
+            {
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              method: "POST",
+              body: JSON.stringify({address: userAddress,signed_message:signedMessage})
+            }
+          );
+          let requestJson = await request.json();
+          console.log(requestJson.data.refresh);
+          localStorage.setItem('token', requestJson.data.token);
+          localStorage.setItem('tokenEndTimestamp', requestJson.data.expiration_timestamp);
+          localStorage.setItem('refreshToken', requestJson.data.refresh);
+        }else if (Date.parse(localStorage.getItem('tokenEndTimestamp'))<Date.now()){
+          //gettoken with refesh-token
+          let requestUrl = `${config.backendApiEntryPoint}token-refresh/`;
+          let request = await fetch(
+            requestUrl,
+            {
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              method: "POST",
+              body: JSON.stringify({refresh: localStorage.getItem('refreshToken')})
+            }
+          );
+          requestJson = await request.json();
+          //write info to localStorage
+          localStorage.setItem('token', requestJson.access);
+          localStorage.setItem('tokenEndTimestamp', (Date.now()/1000)+86400);
+          localStorage.setItem('refreshToken', requestJson.refresh);
+        }
         await web3Provider.send(
           'wallet_switchEthereumChain',
           [{ chainId: "0x1"}],
