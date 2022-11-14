31062e5b4242d5ad319703ebf4105c92479f738d
Denys Osadchuk <beatvin128@gmail.com>
2022-11-14 10:25:36 +0200

    timer fixed

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -240,20 +240,27 @@ export default {
       this.userProgressValue = (this.userBidAmount/this.item.price)*100;
       let timeNow = Date.now()/1000;
       let remTimeInSeconds = this.item.end_date-timeNow;
-      String.prototype.toHHMMSS = function () {
+      String.prototype.toDDHHMMSS = function () {
         var sec_num = parseInt(this, 10); // don't forget the second param
-        var hours = Math.floor(sec_num / 3600);
-        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-        var seconds = sec_num - (hours * 3600) - (minutes * 60);
-
+        var days = Math.floor(sec_num/86400);
+        var hours = Math.floor((sec_num - (days*86400)) / 3600);
+        var minutes = Math.floor((sec_num - ((days*86400)+(hours * 3600))) / 60);
+        // console.log(minutes);
+        var seconds = sec_num -(days*86400)- (hours * 3600) - (minutes * 60);
+        
+        if (days < 10) { days = "0" + days; }
         if (hours < 10) { hours = "0" + hours; }
         if (minutes < 10) { minutes = "0" + minutes; }
         if (seconds < 10) { seconds = "0" + seconds; }
-        return hours + ':' + minutes + ':' + seconds;
+        if (days>0){
+        return days + 'd:' + hours + 'h:' + minutes+'m';
+        }else{
+          return hours + 'h:' + minutes + 'm:' + seconds+'s';
+        }
       }
       if (remTimeInSeconds>0){
 
-        this.remainTimeString = String(remTimeInSeconds).toHHMMSS();
+        this.remainTimeString = String(remTimeInSeconds).toDDHHMMSS();
       }
       else{
         this.remainTimeString = null;
