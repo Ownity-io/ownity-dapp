955ea84a09778d24b8bb9652124d945c21d68cb8
Elena Pohrebniak <68587473+ElenaPohrebniak@users.noreply.github.com>
2022-11-18 11:11:59 +0200

    added vote to listing

---
+++ src/components/listing/ListingFractionMarket.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="tab-contain tab-activities">
+        <div class="table table-activities">
+            <div class="thead">
+                <div class="td">Status</div>
+                <div class="td td-price">Price</div>
+                <div class="td">Pct</div>
+                <div class="td td-button">Date</div>
+            </div>
+                
+            <div class="tr">
+                <div class="td">
+                    <div class="td-wrap td-wrap-sale">
+                        <i class="i-shopping-bag-line"></i>
+                        <span>Sale</span>
+                    </div>
+                </div>
+                <div class="td td-price">
+                    <div class="td-wrap">
+                        <div class="td-wrap-price">
+                            <div class="icon-token"></div> 
+                            <span>12.90 ETH</span>
+                        </div>
+                        <span class="td-light">≈ $ 1000</span>
+                    </div>
+                </div>
+                <div class="td"> 
+                    <a class="td-wrap td-wrap-link" href="" target="_blank" rel="nofollow">
+                        <span>a1565...d48d</span>
+                        <i class="i-external-link-line"></i>
+                    </a> 
+                </div>
+                <div class="td td-date">
+                    <div class="td-button">
+                       <button class="btn btn-td btn-buy">Buy</button>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="table-btn-row">
+                <button class="btn btn-show-more">Show more <i class="i-arrow-down-s-line"></i></button>
+            </div>
+        </div>
+    </div>
+</template>
