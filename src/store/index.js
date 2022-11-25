03c42ff111c09446c70e9ab608c44efca7a56eed
Denys Osadchuk <beatvin128@gmail.com>
2022-11-25 15:15:48 +0200

    added user module to store

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import marketplace from "@/store/modules/marketplace.js";
 import appGlobal from '@/store/modules/appGlobal.js'
 import walletsAndProvider from "./modules/walletsAndProvider";
 import marketplaceListing from "./modules/marketplaceListing";
+import user from './modules/user'
 const store = createStore({
   modules: {
     marketplace,
     appGlobal,
     walletsAndProvider,
-    marketplaceListing
+    marketplaceListing,
+    user
   },
 });
 
