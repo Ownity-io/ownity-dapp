bd59cc0d43e4ebc7f5ab611bcd5f95ebd9ed66cf
Denys Osadchuk <beatvin128@gmail.com>
2022-12-08 10:49:28 +0200

    temporary disabled new buttons on Deposit Modals

--- src/components/modal/DepositPartContinue.vue
+++ src/components/modal/DepositPartContinue.vue
@@ -83,11 +83,12 @@
 
           <!-- v-if="currentPart "  -->
           <div v-if="(currentPart>0 & currentPart!='')" class="modal-desktop-footer">
-            <button disabled class="btn btn-modal-main">Deposit part</button>
+            <!-- <button disabled class="btn btn-modal-main">Deposit part</button> -->
+            <button class="btn btn-modal-main" @click="buyLot">Deposit part</button>
           </div>
 
           <!-- v-else  -->
-          <div v-if="(currentPart>0 & currentPart!='')" class="modal-desktop-footer">
+          <!-- <div v-if="(currentPart>0 & currentPart!='')" class="modal-desktop-footer">
             <button  @click="this.buyLot()" class="btn btn-modal-main">Deposit part</button>
             <button class="btn btn-modal-main">
               <svg class="loader" viewBox="0 0 18 18"  xmlns="http://www.w3.org/2000/svg">
@@ -102,18 +103,19 @@
                 15.9999 10.6196 16 9.00009H18C18 11.0823 17.278 13.1001 15.957 14.7096C14.6361 16.3192 12.7979 17.4209 10.7557 17.8271C8.71355 18.2333 6.5937 17.9188 4.75737 16.9373C2.92104 15.9557 1.48187 14.3678 0.685061 12.4441C-0.111747 10.5204 -0.216886 8.37992 0.387558 6.38739C0.992002 4.39486 2.26863 2.67355 3.99992 1.51675C5.73121 0.359959 7.81004 -0.160747 9.88221 0.0433572C11.9544 0.247462 13.8917 1.16375 15.364 2.63609V2.63609Z" fill="white"/>
               </svg>
             </button>
-          </div>
+          </div> -->
           
         </div>
       </div>
             
       <!-- v-if="currentPart "  -->
       <div v-if="(currentPart>0 & currentPart!='')" class="modal-mobile-footer">
-        <button disabled class="btn btn-modal-main">Deposit part</button>
+        <!-- <button disabled class="btn btn-modal-main">Deposit part</button> -->
+        <button class="btn btn-modal-main" @click="buyLot">Deposit part</button>
       </div>
 
       <!-- v-else  -->
-      <div v-if="(currentPart>0 & currentPart!='')" class="modal-mobile-footer">
+      <!-- <div v-if="(currentPart>0 & currentPart!='')" class="modal-mobile-footer">
         <button  @click="this.buyLot()" class="btn btn-modal-main">Deposit part</button>
         <button class="btn btn-modal-main">
           <svg class="loader" viewBox="0 0 18 18"  xmlns="http://www.w3.org/2000/svg">
@@ -128,7 +130,7 @@
             15.9999 10.6196 16 9.00009H18C18 11.0823 17.278 13.1001 15.957 14.7096C14.6361 16.3192 12.7979 17.4209 10.7557 17.8271C8.71355 18.2333 6.5937 17.9188 4.75737 16.9373C2.92104 15.9557 1.48187 14.3678 0.685061 12.4441C-0.111747 10.5204 -0.216886 8.37992 0.387558 6.38739C0.992002 4.39486 2.26863 2.67355 3.99992 1.51675C5.73121 0.359959 7.81004 -0.160747 9.88221 0.0433572C11.9544 0.247462 13.8917 1.16375 15.364 2.63609V2.63609Z" fill="white"/>
           </svg>
         </button>
-      </div>
+      </div> -->
       
     </div>
   </div>
