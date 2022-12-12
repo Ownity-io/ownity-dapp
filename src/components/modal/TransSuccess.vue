639922996674239f251cd416bbcf0588b1074561
Denys Osadchuk <beatvin128@gmail.com>
2022-12-12 09:51:40 +0200

    added success transaction modal on start collecting

--- src/components/modal/TransSuccess.vue
+++ src/components/modal/TransSuccess.vue
@@ -3,7 +3,7 @@
     <div class="modal-wrapper">
       <div class="modal-header">
         <!-- <div class="modal-name">Connect Your Wallet</div> -->
-        <button class="btn-close" >
+        <button class="btn-close" @click="reloadPage">
           <i class="i-close-line"></i>
         </button>
       </div>
@@ -29,8 +29,9 @@
                       </div>
                   </div>
                   <div class="td"> 
-                      <a class="td-wrap td-wrap-link" href="" target="_blank" rel="nofollow">
-                          <span>a1565...d48d</span>
+                      <a class="td-wrap td-wrap-link" :href="config.etherscanTxUrlStart+this.$store.getters['appGlobal/getLastTransactionHash']" target="_blank" rel="nofollow">
+                          <!-- <span>a1565...d48d</span> -->
+                          <span>{{this.$store.getters['appGlobal/getLastTransactionHash'].substring(0,6)}}...{{this.$store.getters['appGlobal/getLastTransactionHash'].substring(62,66)}}</span>
                           <i class="i-external-link-line"></i>
                       </a> 
                   </div>
@@ -45,11 +46,12 @@
 
           <!-- v-if="currentPart "  -->
           <div class="modal-desktop-footer">
-            <button disabled class="btn btn-modal-main">Done</button>
+            <!-- <button disabled class="btn btn-modal-main" @click="reloadPage">Done</button> -->
+            <button class="btn btn-modal-main" @click="reloadPage">Done</button>
           </div>
 
           <!-- v-else  -->
-          <div class="modal-desktop-footer">
+          <!-- <div class="modal-desktop-footer">
             <button class="btn btn-modal-main">Done</button>
             <button class="btn btn-modal-main">
               <svg class="loader" viewBox="0 0 18 18"  xmlns="http://www.w3.org/2000/svg">
@@ -64,18 +66,19 @@
                 15.9999 10.6196 16 9.00009H18C18 11.0823 17.278 13.1001 15.957 14.7096C14.6361 16.3192 12.7979 17.4209 10.7557 17.8271C8.71355 18.2333 6.5937 17.9188 4.75737 16.9373C2.92104 15.9557 1.48187 14.3678 0.685061 12.4441C-0.111747 10.5204 -0.216886 8.37992 0.387558 6.38739C0.992002 4.39486 2.26863 2.67355 3.99992 1.51675C5.73121 0.359959 7.81004 -0.160747 9.88221 0.0433572C11.9544 0.247462 13.8917 1.16375 15.364 2.63609V2.63609Z" fill="white"/>
               </svg>
             </button>
-          </div>
+          </div> -->
 
         </div>
       </div>
             
       <!-- v-if="currentPart "  -->
       <div  class="modal-mobile-footer">
-        <button disabled class="btn btn-modal-main">Done</button>
+        <!-- <button disabled class="btn btn-modal-main" @click="reloadPage">Done</button> -->
+        <button class="btn btn-modal-main" @click="reloadPage">Done</button>
       </div>
 
       <!-- v-else  -->
-      <div   class="modal-mobile-footer">
+      <!-- <div   class="modal-mobile-footer">
         <button   class="btn btn-modal-main">Done</button>
         <button class="btn btn-modal-main">
           <svg class="loader" viewBox="0 0 18 18"  xmlns="http://www.w3.org/2000/svg">
@@ -90,8 +93,26 @@
             15.9999 10.6196 16 9.00009H18C18 11.0823 17.278 13.1001 15.957 14.7096C14.6361 16.3192 12.7979 17.4209 10.7557 17.8271C8.71355 18.2333 6.5937 17.9188 4.75737 16.9373C2.92104 15.9557 1.48187 14.3678 0.685061 12.4441C-0.111747 10.5204 -0.216886 8.37992 0.387558 6.38739C0.992002 4.39486 2.26863 2.67355 3.99992 1.51675C5.73121 0.359959 7.81004 -0.160747 9.88221 0.0433572C11.9544 0.247462 13.8917 1.16375 15.364 2.63609V2.63609Z" fill="white"/>
           </svg>
         </button>
-      </div>
+      </div> -->
       
     </div>
   </div>
 </template>
+
+<script>
+import config from '@/config';
+export default{
+
+  methods:{
+    reloadPage(){
+      location.reload();
+    }
+  },
+  data(){
+    return{
+      config:config
+    }    
+  }
+
+}
+</script>
