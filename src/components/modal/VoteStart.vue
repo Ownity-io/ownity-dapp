8f70f86d8c54f306eac864eacb1941287a369098
Denys Osadchuk <beatvin128@gmail.com>
2022-12-12 11:10:09 +0200

    added tx success modal call to voting start modal

--- src/components/modal/VoteStart.vue
+++ src/components/modal/VoteStart.vue
@@ -61,14 +61,13 @@
           </div>
 
           <!-- v-if="currentPart "  -->
-          <div class="modal-desktop-footer">
-            <!-- <button disabled class="btn btn-modal-main">Start vote</button> -->
-            <button class="btn btn-modal-main" @click="startVote">Start vote</button>
+          <div class="modal-desktop-footer" v-if="!buttonWaiting">
+            <button class="btn btn-modal-main" @click="startVote" v-if="(this.amount>0 & this.checkedMarketplace!=null)">Start vote</button>
+            <button disabled class="btn btn-modal-main" v-else>Start vote</button>
           </div>
 
           <!-- v-else  -->
-          <!-- <div class="modal-desktop-footer">
-            <button class="btn btn-modal-main" @click="startVote">Start vote</button>
+          <div class="modal-desktop-footer" v-if="buttonWaiting">
             <button class="btn btn-modal-main">
               <svg class="loader" viewBox="0 0 18 18"  xmlns="http://www.w3.org/2000/svg">
                 <path d="M15.364 2.63609L13.95 4.05009C12.8049 2.90489 
@@ -82,20 +81,19 @@
                 15.9999 10.6196 16 9.00009H18C18 11.0823 17.278 13.1001 15.957 14.7096C14.6361 16.3192 12.7979 17.4209 10.7557 17.8271C8.71355 18.2333 6.5937 17.9188 4.75737 16.9373C2.92104 15.9557 1.48187 14.3678 0.685061 12.4441C-0.111747 10.5204 -0.216886 8.37992 0.387558 6.38739C0.992002 4.39486 2.26863 2.67355 3.99992 1.51675C5.73121 0.359959 7.81004 -0.160747 9.88221 0.0433572C11.9544 0.247462 13.8917 1.16375 15.364 2.63609V2.63609Z" fill="white"/>
               </svg>
             </button>
-          </div> -->
+          </div>
 
         </div>
       </div>
             
       <!-- v-if="currentPart "  -->
-      <div  class="modal-mobile-footer">
-        <!-- <button disabled class="btn btn-modal-main">Start vote</button> -->
-        <button class="btn btn-modal-main" @click="startVote">Start vote</button>
+      <div  class="modal-mobile-footer" v-if="!buttonWaiting">
+        <button class="btn btn-modal-main" @click="startVote" v-if="(this.amount>0 & this.checkedMarketplace!=null)">Start vote</button>
+        <button disabled class="btn btn-modal-main" v-else>Start vote</button>
       </div>
 
       <!-- v-else  -->
-      <!-- <div   class="modal-mobile-footer">
-        <button   class="btn btn-modal-main" @click="startVote">Start vote</button>
+      <div   class="modal-mobile-footer" v-if="buttonWaiting">
         <button class="btn btn-modal-main">
           <svg class="loader" viewBox="0 0 18 18"  xmlns="http://www.w3.org/2000/svg">
             <path d="M15.364 2.63609L13.95 4.05009C12.8049 2.90489 
@@ -109,7 +107,7 @@
             15.9999 10.6196 16 9.00009H18C18 11.0823 17.278 13.1001 15.957 14.7096C14.6361 16.3192 12.7979 17.4209 10.7557 17.8271C8.71355 18.2333 6.5937 17.9188 4.75737 16.9373C2.92104 15.9557 1.48187 14.3678 0.685061 12.4441C-0.111747 10.5204 -0.216886 8.37992 0.387558 6.38739C0.992002 4.39486 2.26863 2.67355 3.99992 1.51675C5.73121 0.359959 7.81004 -0.160747 9.88221 0.0433572C11.9544 0.247462 13.8917 1.16375 15.364 2.63609V2.63609Z" fill="white"/>
           </svg>
         </button>
-      </div> -->
+      </div>
       
     </div>
   </div>
@@ -132,6 +130,7 @@ export default {
       config:config,
       ABI:ABI,
       provider:null,
+      buttonWaiting:false
     };
   },
   async mounted(){    
@@ -188,46 +187,61 @@ export default {
       return value * 10**this.item.currency.decimals;
     },
     async startVote() {
+      this.buttonWaiting=true;
       if (this.amount>0 & this.checkedMarketplace!=null) {
-        let signed_message = await this.$store.dispatch('walletsAndProvider/signMessageWithGlobalProvider',
-          `${this.checkedMarketplace}-${this.item.id}-${this.item.currency.address}-${this.noExponents(this.amount * 10 ** this.item.currency.decimals)}-${this.item.end_date}`);
-        console.log(signed_message);
-        let requestLink = `${config.backendApiEntryPoint}voting-create/`;
-        let requestOptions = {
-          method: "POST",
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-          body: JSON.stringify({
-            "marketplace_id": this.checkedMarketplace,
-            "lot_id": this.item.id,
-            "currency": this.item.currency.address,
-            "amount": this.noExponents(this.amount * 10 ** this.item.currency.decimals),
-            "end_date": this.item.end_date,
-            "signed_message": signed_message,
-            "type": "SELL",
-            "blockchain":this.item.collection.blockchain
-          })
-        };
-        let request = await fetch(requestLink, requestOptions);
-        let requestJson = await request.json();
-        console.log(requestJson);
-        if (requestJson.success) {
-          console.log('OK');
-          if (parseInt((requestJson.voting_percentage.replace('%', ''))) >= 51) {
-            console.log('OK2');
-            console.log(requestJson.voting_id);
-            await this.sellLot(requestJson.voting_id)
+        try {
+          let signed_message = await this.$store.dispatch('walletsAndProvider/signMessageWithGlobalProvider',
+            `${this.checkedMarketplace}-${this.item.id}-${this.item.currency.address}-${this.noExponents(this.amount * 10 ** this.item.currency.decimals)}-${this.item.end_date}`);
+          console.log(signed_message);
+          let requestLink = `${config.backendApiEntryPoint}voting-create/`;
+          let requestOptions = {
+            method: "POST",
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+            body: JSON.stringify({
+              "marketplace_id": this.checkedMarketplace,
+              "lot_id": this.item.id,
+              "currency": this.item.currency.address,
+              "amount": this.noExponents(this.amount * 10 ** this.item.currency.decimals),
+              "end_date": this.item.end_date,
+              "signed_message": signed_message,
+              "type": "SELL",
+              "blockchain": this.item.collection.blockchain
+            })
+          };
+          let request = await fetch(requestLink, requestOptions);
+          let requestJson = await request.json();
+          console.log(requestJson);
+          if (requestJson.success) {
+            console.log('OK');
+            if (parseInt((requestJson.voting_percentage.replace('%', ''))) >= 51) {
+              console.log('OK2');
+              console.log(requestJson.voting_id);
+              try{
+                await this.sellLot(requestJson.voting_id)
+              }
+              catch{
+                this.buttonWaiting=false;
+                alert('Error!');
+              }
+            }
+            else {
+              location.reload();
+            }
           }
-          else{
-            location.reload();  
-          }          
+          else {
+            this.buttonWaiting=false;
+            alert('Error!');
+          }
         }
-        else {
+        catch{
+          this.buttonWaiting=false;
           alert('Error!');
         }
+        
       }
     },
     noExponents (value) {
@@ -291,7 +305,13 @@ export default {
       console.log(sellLot);
       let trx = await (toRaw(this.provider)).waitForTransaction(sellLot.hash);
       if (trx.status == 1) {
-        location.reload();
+        await this.$store.dispatch('appGlobal/setLastTransactionHash', declineBid.hash);
+        await this.$store.dispatch('appGlobal/setShowStartVotingModal', false);
+        await this.$store.dispatch('appGlobal/setShowTransSuccessModal', true);
+      }
+      else{
+        this.buttonWaiting=false;
+          alert('Error!');
       }
       
     }
