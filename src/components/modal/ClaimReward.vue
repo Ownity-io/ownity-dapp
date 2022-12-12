051a9b614958c994ef9edde259dd65290433433d
Denys Osadchuk <beatvin128@gmail.com>
2022-12-12 11:50:01 +0200

    added tx modal call to claim and claimReward modals

--- src/components/modal/ClaimReward.vue
+++ src/components/modal/ClaimReward.vue
@@ -39,14 +39,12 @@
           </div>
 
           <!-- v-if="currentPart "  -->
-          <div  class="modal-desktop-footer">
-            <!-- <button disabled class="btn btn-modal-main">Claim reward</button> -->
+          <div  class="modal-desktop-footer" v-if="!buttonWaiting">
             <button class="btn btn-modal-main" @click="claimReward">Claim reward</button>
           </div>
 
           <!-- v-else  -->
-          <!-- <div   class="modal-desktop-footer">
-            <button   class="btn btn-modal-main">Claim reward</button>
+          <div   class="modal-desktop-footer" v-if="buttonWaiting">
             <button class="btn btn-modal-main">
               <svg class="loader" viewBox="0 0 18 18"  xmlns="http://www.w3.org/2000/svg">
                 <path d="M15.364 2.63609L13.95 4.05009C12.8049 2.90489 
@@ -60,20 +58,19 @@
                 15.9999 10.6196 16 9.00009H18C18 11.0823 17.278 13.1001 15.957 14.7096C14.6361 16.3192 12.7979 17.4209 10.7557 17.8271C8.71355 18.2333 6.5937 17.9188 4.75737 16.9373C2.92104 15.9557 1.48187 14.3678 0.685061 12.4441C-0.111747 10.5204 -0.216886 8.37992 0.387558 6.38739C0.992002 4.39486 2.26863 2.67355 3.99992 1.51675C5.73121 0.359959 7.81004 -0.160747 9.88221 0.0433572C11.9544 0.247462 13.8917 1.16375 15.364 2.63609V2.63609Z" fill="white"/>
               </svg>
             </button>
-          </div> -->
+          </div>
 
         </div>
       </div>
 
             
       <!-- v-if="currentPart "  -->
-      <div  class="modal-mobile-footer">
+      <div  class="modal-mobile-footer" v-if="!buttonWaiting">
         <button class="btn btn-modal-main" @click="claimReward">Claim reward</button>
       </div>
 
       <!-- v-else  -->
-      <div   class="modal-mobile-footer">
-        <button   class="btn btn-modal-main">Claim reward</button>
+      <div   class="modal-mobile-footer" v-if="buttonWaiting">
         <button class="btn btn-modal-main">
           <svg class="loader" viewBox="0 0 18 18"  xmlns="http://www.w3.org/2000/svg">
             <path d="M15.364 2.63609L13.95 4.05009C12.8049 2.90489 
@@ -107,7 +104,8 @@ export default {
       ABI:ABI,
       config:config,
       currencyToUsdPrice:1,
-      userBidAmount:0
+      userBidAmount:0,
+      buttonWaiting:false
     };
   },
   async mounted(){
@@ -119,6 +117,7 @@ export default {
   },
   methods:{
     async claimReward(){  
+      this.buttonWaiting = true; 
       const contract = new ethers.Contract(this.config.contractAddress, this.ABI.abi,await (toRaw(this.provider)).getSigner());
       try{
         let claimReward = await contract.claimReward(
@@ -127,14 +126,18 @@ export default {
         );
         let trx = await (toRaw(this.provider)).waitForTransaction(claimReward.hash);
         if (trx.status==1){
-          location.reload();
+          await this.$store.dispatch('appGlobal/setLastTransactionHash', claimReward.hash);
+          await this.$store.dispatch('appGlobal/setShowClaimRewardModal', false);
+          await this.$store.dispatch('appGlobal/setShowTransSuccessModal', true);
         }
         else{
+          this.buttonWaiting = false;
           alert('Error!')
-        }          
+        }           
       }
       catch{
-        alert('Error');
+        this.buttonWaiting = false;
+          alert('Error!')
       }
     },
     abbrNum(number, decPlaces) {
