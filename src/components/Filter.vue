ea858ffe51b6cf47ce01bf84294a224633819d92
Denys Osadchuk <denys.osadcuk@elephantslab.com>
2022-12-21 11:53:13 +0200

    removed old comments from filters

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="filter">
-
-    <!--  ------------------ NEW : switcher-------------------- -->
     <div class="filter-section" :class="{ 'collapse-section': filterSection0 }" v-if="this.$route.name=='Collection' ||this.$route.name=='Marketplace'">
       <button class="filter-section-name" @click="filterSection0 = !filterSection0">
         <span>Status</span>
@@ -19,26 +17,6 @@
         </li>
       </ul>
     </div>
-
-    <!-- FOR DEMO -->
-    <!-- <div class="filter-section" :class="{ 'collapse-section': filterSection0 }" v-if="this.$route.name=='Collection' ||this.$route.name=='Marketplace'">
-      <button class="filter-section-name" @click="filterSection0 = !filterSection0">
-        <span>Status</span>
-        <i class="i-arrow-up-s-line"></i>
-      </button>
-      <ul class="filter-ul">
-        <li class="filter-li" >
-          <div class="input-checkbox input-switcher">
-            <input type="checkbox" id="input-switch" v-model="currentlyGathering" @change="fetchAndSetListingsStartInfo"/>
-            <label for="input-switch">
-              <span>Live gather</span>
-              <div class="input-switch"></div>
-            </label>
-          </div>
-        </li>
-      </ul>
-    </div> -->
-
     <div class="filter-section" :class="{ 'collapse-section': filterSection0 }" v-if="this.$route.name=='Profile' & (this.onlyFav||(!this.onlyFav & !this.vote))">
       <button class="filter-section-name" @click="filterSection0 = !filterSection0">
         <span>Status</span>
