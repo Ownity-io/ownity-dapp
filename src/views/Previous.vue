505f8500a4b3fbc30dadbd0028b3d0d81ba3afa2
vladislavqq <vladyslav@elephantslab.com>
2022-12-22 17:47:14 +0200

    fully second animtion

--- src/views/Previous.vue
+++ src/views/Previous.vue
@@ -9,6 +9,9 @@
                         <section class="previous-first-screen">
                            <first-block/>
                         </section>
+                        <section class="previous-second-screen">
+                          <second-block/>
+                        </section>
                     </div>
                 </main>
             </div>
@@ -25,9 +28,11 @@ import '../assets/styles/previous.css'
 import FirstBlock from "@/components/Previous/FirstBlock.vue";
 import HeaderPrevious from '@/components/Previous/HeaderPrevious.vue';
 import FooterComponent from '@/components/FooterComponent.vue'
+import SecondBlock from "@/components/Previous/SecondBlock.vue";
 
 export default {
 	components: {
+		SecondBlock,
 		FirstBlock,
         HeaderPrevious,
         FooterComponent
