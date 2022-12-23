29f703fbca80bea6cdf2c46827f9feb2d391eb1e
Elena Pohrebniak <olena@elephantslab.com>
2022-12-23 18:04:58 +0200

    added block with card (PREVIOUS)

--- src/views/Previous.vue
+++ src/views/Previous.vue
@@ -12,13 +12,31 @@
                         <section class="previous-second-screen">
                           <second-block/>
                         </section>
+                        <section class="previous-cards-screen">
+                            <div class="container">
+                                <div class="previous-screen-header">
+                                    <div class="previous-title">Open Fraction Deals</div>
+                                <div class="previous-describe">
+                                    The best open deals you can take part in right now. 
+                                    Join a deal that's already started, contribute your part, and become an official owner.
+                                </div>
+                            <div class="previous-link">Learn more about open deals</div>
+                                </div>
+                                <div class="previous-cards-list">
+                                    <div class="cards-container">
+                                        <PreviousCard />
+                                    </div>
+                                    <button class="btn btn-previous">See All Deals</button>
+                                </div>
+                            </div>
+                        </section>
                     </div>
                 </main>
             </div>
 
         </div>
 
-        <FooterComponent/>
+        <FooterPrevious/>
     </div>
 </template>
 
@@ -27,15 +45,17 @@
 import '../assets/styles/previous.css'
 import FirstBlock from "@/components/Previous/FirstBlock.vue";
 import HeaderPrevious from '@/components/Previous/HeaderPrevious.vue';
-import FooterComponent from '@/components/FooterComponent.vue'
+import FooterPrevious from '@/components/Previous/FooterPrevious.vue'
 import SecondBlock from "@/components/Previous/SecondBlock.vue";
+import PreviousCard from "@/components/Previous/PreviousCard.vue";
 
 export default {
 	components: {
 		SecondBlock,
 		FirstBlock,
         HeaderPrevious,
-        FooterComponent
+        FooterPrevious,
+        PreviousCard
 	},
     data() {
         return{
