bdb22a8f7faa679fafd9ae8164a205e818b8e9ab
vladislavqq <vladyslav@elephantslab.com>
2022-12-23 18:38:09 +0200

    comit text animation

--- src/components/Previous/FirstBlock.vue
+++ src/components/Previous/FirstBlock.vue
@@ -2,73 +2,30 @@
   <div ref="intro" class="intros">
 <!--    <canvas id="hero-lightpass"/>-->
 <!--    <video ref="video" src="src/assets/home2 без лого.mp4"></video>-->
-   
+
   </div>
 </template>
 
+
 <script>
+
+import lottie from 'lottie-web'
 
 export default {
 	name: "FirstBlock",
   data() {
 		return {
-
     }
   },
 	mounted() {
-		// window.addEventListener('scroll', function (){
-		// 	const html = document.documentElement;
-		// 	const canvas = document.getElementById("hero-lightpass");
-		// 	const context = canvas.getContext("2d");
-    //
-		// 	const frameCount = 30;
-		// 	const currentFrame = index => (
-		// 		// `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
-		// 		`/src/assets/animationFirst/ezgif-frame-${index.toString().padStart(3, '0')}.png`
-		// 	)
-    //
-		// 	const preloadImages = () => {
-		// 		for (let i = 1; i < frameCount; i++) {
-		// 			const img = new Image();
-		// 			img.src = currentFrame(i);
-		// 		}
-		// 	};
-    //
-		// 	const img = new Image()
-		// 	img.src = currentFrame(1);
-		// 	canvas.width=window.innerWidth;
-		// 	canvas.height=window.innerHeight;
-		// 	img.onload=function(){
-		// 		context.drawImage(img, 0, 0);
-		// 	}
-    //
-		// 	const updateImage = index => {
-		// 		img.src = currentFrame(index);
-		// 		context.drawImage(img, 0, 0);
-		// 	}
-    //
-		// 	window.addEventListener('scroll', () => {
-		// 		const scrollTop = html.scrollTop;
-		// 		const maxScrollTop = html.scrollHeight - window.innerHeight;
-		// 		const scrollFraction = scrollTop / maxScrollTop;
-		//  		const frameIndex = Math.min(
-		//  			frameCount - 1,
-		//  			Math.ceil(scrollFraction * frameCount)
-		//  		);
-    //
-		//  		requestAnimationFrame(() => updateImage(frameIndex + 1))
-		//  	});
-    //
-		//  	preloadImages()
-		//  })
+		lottie.loadAnimation({
+      container: this.$refs.intro,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: './src/assets/sun.json'
+    })
 	}
 }
 
 </script>
-
-<style scoped>
-canvas {
-    max-width: 100vw;
-    max-height: 100vh;
-}
-</style>
--- src/components/Previous/SecondBlock.vue
+++ src/components/Previous/SecondBlock.vue
@@ -1,78 +1,78 @@
 <template>
-  <div class="fractional" >
-    <div class="fractional-left">
-      <p class="fractional-title">Fractional Ownership</p>
-      <p class="fractional-subtitle">
-        The mechanics of fractional ownership enable users to purchase an NFT in a group.
-        Each user can contribute the required part and start or maintain the fundraising for the listing purchase.
-      </p>
-      <p class="fractional-options-title" >
-        Flexible interaction options
-      </p>
-      <div class="fractional-options">
-        <div class="fractional-inner">
-          <div><div class="design"></div><p>Buy 1% to 100% of NFT</p></div>
-          <div ref="fractional"><div class="design"></div><p>Make decisions in DAO mode</p></div>
-        </div>
-        <div class="fractional-inner">
-          <div><div class="design"></div><p>Sell your part at any time</p></div>
-          <div><div class="design"></div><p>Explore profitable listings</p></div>
-        </div>
-      </div>
-    </div>
-    <div class="fractional-right">
-      <div class="factional-nft square-transition" ref="square">
-        <div class="factional-nft-1">
-          <span class="nft-procent">
-            <p class="nft-procent-title">10% (≈$7.000)</p>
-            <span class="nft-procent-user">
-              <span class="nft-procent-user-icon"></span>
-              <p class="nft-procent-user-name">Aaron Stuffs</p>
-            </span>
-          </span>
-        </div>
-        <div class="factional-nft-2">
-          <span class="nft-procent">
-            <p class="nft-procent-title">10% (≈$7.000)</p>
-            <span class="nft-procent-user">
-              <span class="nft-procent-user-icon"></span>
-              <p class="nft-procent-user-name">Star Muffin</p>
-            </span>
-          </span>
+  <div class="fractional">
+<!--    <div class="fractional-left">-->
+<!--      <p class="fractional-title">Fractional Ownership</p>-->
+<!--      <p class="fractional-subtitle">-->
+<!--        The mechanics of fractional ownership enable users to purchase an NFT in a group.-->
+<!--        Each user can contribute the required part and start or maintain the fundraising for the listing purchase.-->
+<!--      </p>-->
+<!--      <p class="fractional-options-title" >-->
+<!--        Flexible interaction options-->
+<!--      </p>-->
+<!--      <div class="fractional-options">-->
+<!--        <div class="fractional-inner">-->
+<!--          <div><div class="design"></div><p>Buy 1% to 100% of NFT</p></div>-->
+<!--          <div ref="fractional"><div class="design"></div><p>Make decisions in DAO mode</p></div>-->
+<!--        </div>-->
+<!--        <div class="fractional-inner">-->
+<!--          <div><div class="design"></div><p>Sell your part at any time</p></div>-->
+<!--          <div><div class="design"></div><p>Explore profitable listings</p></div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="fractional-right">-->
+<!--      <div class="factional-nft square-transition" ref="square">-->
+<!--        <div class="factional-nft-1">-->
+<!--          <span class="nft-procent">-->
+<!--            <p class="nft-procent-title">10% (≈$7.000)</p>-->
+<!--            <span class="nft-procent-user">-->
+<!--              <span class="nft-procent-user-icon"></span>-->
+<!--              <p class="nft-procent-user-name">Aaron Stuffs</p>-->
+<!--            </span>-->
+<!--          </span>-->
+<!--        </div>-->
+<!--        <div class="factional-nft-2">-->
+<!--          <span class="nft-procent">-->
+<!--            <p class="nft-procent-title">10% (≈$7.000)</p>-->
+<!--            <span class="nft-procent-user">-->
+<!--              <span class="nft-procent-user-icon"></span>-->
+<!--              <p class="nft-procent-user-name">Star Muffin</p>-->
+<!--            </span>-->
+<!--          </span>-->
 
-        </div>
-        <div class="factional-nft-3">
-          <span class="nft-procent">
-            <p class="nft-procent-title">10% (≈$7.000)</p>
-            <span class="nft-procent-user">
-              <span class="nft-procent-user-icon"></span>
-              <p class="nft-procent-user-name">Star Muffin</p>
-            </span>
-          </span>
+<!--        </div>-->
+<!--        <div class="factional-nft-3">-->
+<!--          <span class="nft-procent">-->
+<!--            <p class="nft-procent-title">10% (≈$7.000)</p>-->
+<!--            <span class="nft-procent-user">-->
+<!--              <span class="nft-procent-user-icon"></span>-->
+<!--              <p class="nft-procent-user-name">Star Muffin</p>-->
+<!--            </span>-->
+<!--          </span>-->
 
-        </div>
-        <div class="factional-nft-4">
-          <span class="nft-procent">
-            <p class="nft-procent-title">15% (≈$13.500)</p>
-            <span class="nft-procent-user">
-              <span class="nft-procent-user-icon"></span>
-              <p class="nft-procent-user-name">Sammy</p>
-            </span>
-          </span>
+<!--        </div>-->
+<!--        <div class="factional-nft-4">-->
+<!--          <span class="nft-procent">-->
+<!--            <p class="nft-procent-title">15% (≈$13.500)</p>-->
+<!--            <span class="nft-procent-user">-->
+<!--              <span class="nft-procent-user-icon"></span>-->
+<!--              <p class="nft-procent-user-name">Sammy</p>-->
+<!--            </span>-->
+<!--          </span>-->
 
-        </div>
-        <div class="factional-nft-5">
-          <span class="nft-procent">
-            <p class="nft-procent-title">10% (≈$7.000)</p>
-            <span class="nft-procent-user">
-              <span class="nft-procent-user-icon"></span>
-              <p class="nft-procent-user-name">Aaron Stuffs</p>
-            </span>
-          </span>
+<!--        </div>-->
+<!--        <div class="factional-nft-5">-->
+<!--          <span class="nft-procent">-->
+<!--            <p class="nft-procent-title">10% (≈$7.000)</p>-->
+<!--            <span class="nft-procent-user">-->
+<!--              <span class="nft-procent-user-icon"></span>-->
+<!--              <p class="nft-procent-user-name">Aaron Stuffs</p>-->
+<!--            </span>-->
+<!--          </span>-->
 
-        </div>
-      </div>
-    </div>
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
 </template>
 
