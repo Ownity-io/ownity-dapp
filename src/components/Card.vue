073186ba335b80d9dbac72b59b7161f054eac611
Denys Osadchuck <denys.osadchuck@elephantslab.com>
2022-12-26 16:13:34 +0200

    changed Method ToSetMarketplaceLink

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -481,10 +481,11 @@ export default {
     },
     setLinkToMarketplacePage(){
       let exampleStr = this.item.marketplace.listing_link;
-      let collection_address = this.item.collection.contract_address;
       let token_id = this.item.token_id;
-
-      this.linkToMarketplacePage = eval('`'+exampleStr+'`');
+      let collection_address = this.item.collection.contract_address;      
+      exampleStr = exampleStr.replace('${collection_address}',collection_address);
+      exampleStr = exampleStr.replace('${token_id}',token_id);
+      this.linkToMarketplacePage = exampleStr;
     },
     toFixedIfNecessary(value, dp) {
       return +parseFloat(value).toFixed(dp);
