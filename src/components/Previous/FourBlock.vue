975892b8bb2fabfa32afd6415261d6b655294feb
Denys Osadchuck <denys.osadchuck@elephantslab.com>
2022-12-29 17:24:25 +0200

    added 10 collections to previous page

--- src/components/Previous/FourBlock.vue
+++ src/components/Previous/FourBlock.vue
@@ -26,72 +26,103 @@
           </div>
         </div>
 
-        <div class="tr">
-
+        <div class="tr" v-for="collection in collections.results" :key="collection" v-if="collections">
+        
           <div class="td td-collection">
             <div class="td-wrap td-wrap-collection">
-              <div class="collection-img"></div>
-              <a class="collection-data">
-                <span class="collection-id">name</span>
+              <div class="collection-img" :style="{ backgroundImage: `url(${collection.logo})` }"></div>
+              <a class="collection-data" :href="'/collection/' + collection.contract_address">
+                <span class="collection-id">{{ collection.name }}</span>
               </a>
             </div>
           </div>
-
+        
           <div class="td td-price">
             <div class="td-wrap">
               <div class="td-wrap-price">
                 <div class="icon-token"></div>
-                <span>1000 ETH</span>
+                <span>{{ abbrNum(toFixedIfNecessary(collection.volume_all, 2), 2) }} ETH</span>
               </div>
               <!-- <span class="td-light">≈ $ 1000</span> -->
             </div>
           </div>
-
+        
           <div class="td td-price">
             <div class="td-wrap">
               <div class="td-wrap-price">
                 <div class="icon-token"></div>
-                <span>1000 ETH</span>
+                <span>{{ abbrNum(toFixedIfNecessary(collection.floor_price, 2), 0) }} ETH</span>
               </div>
               <!-- <span class="td-light">≈ $ 1000</span> -->
             </div>
           </div>
-
+        
           <div class="td td-data">
             <div class="td-wrap">
-              holders
+              {{ abbrNum(collection.holders, 1) }}
             </div>
           </div>
-
+        
           <div class="td td-data">
             <div class="td-wrap">
-              total sup
+              {{ abbrNum(collection.total_supply, 1) }}
             </div>
           </div>
-
+        
         </div>
       </div>
-      <div class="collection-design">
+      <!-- <div class="collection-design">
         <button class="btn btn-previous">
           See All Collections
         </button>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 
 <script>
-
+import config from '@/config.json'
 export default {
 	name: "FourBlock",
   data() {
 		return {
-
+      collections:null,
+      render:false,
+      config:config
     }
+  },
+  async mounted(){
+    await this.fetchAndSetNftCollections();
+    this.render = true;
+  },
+  methods:{
+    async fetchAndSetNftCollections() {
+      let requestUrl = `${config.backendApiEntryPoint}nft-collections/?limit=${config.collectionsPerPage}`;
+      console.log(requestUrl);
+      let request = await fetch(requestUrl);
+      let requestJson = await request.json();
+      this.collections = requestJson;
+    },
+    toFixedIfNecessary(value, dp) {
+      return +parseFloat(value).toFixed(dp);
+    },
+    abbrNum(number, decPlaces) {
+      decPlaces = Math.pow(10, decPlaces);
+      var abbrev = ["k", "m", "b", "t"];
+      for (var i = abbrev.length - 1; i >= 0; i--) {
+        var size = Math.pow(10, (i + 1) * 3);
+        if (size <= number) {
+          number = Math.round(number * decPlaces / size) / decPlaces;
+          if ((number == 1000) && (i < abbrev.length - 1)) {
+            number = 1;
+            i++;
+          }
+          number += abbrev[i];
+          break;
+        }
+      }
+      return number;
+    },
   }
 }
 </script>
-
-<style scoped>
-
-</style>
