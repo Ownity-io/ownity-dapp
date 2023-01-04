8176f29407719ae0fbd98e8552217dc05575804a
Elena Pohrebniak <olena@elephantslab.com>
2023-01-04 18:20:47 +0200

    added pop-up fraction

---
+++ src/components/modal/FractionActivity.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="modal">
+    <div class="modal-wrapper modal-claim">
+      <div class="modal-header">
+        <div class="modal-name">
+            {{ translatesGet('FRACTION_ACTIVITY') }}
+        </div>
+        <button class="btn-close">
+          <i class="i-close-line"></i>
+        </button>
+      </div>
+
+      <div class="modal-content">
+        <div class="modal-container">
+            <div class="modal-members">
+                <i class="i-account-circle-line"></i>
+                {{translatesGet('MEMBERS_VOTED')}}: <span>1/6</span>
+            </div>
+            <div class="modal-table">
+
+            <div class="tab-contain tab-activities">
+                <div class="table table-activities">
+                    <div class="thead">
+                        <div class="td">{{translatesGet('ACTIVITY_THEAD-2')}}</div>
+                        <div class="td td-price">{{translatesGet('ACTIVITY_THEAD-3')}}</div>
+                        <div class="td">{{translatesGet('ACTIVITY_THEAD-4')}}</div>
+                    </div>
+                        
+                    <div class="tr">
+                        <div class="td td-category">
+                            <div class="td-wrap td-wrap-category">
+                                <i class="i-shopping-bag-line"></i>
+                                <span>{{translatesGet('STATUS-SALE')}}</span>
+                            </div>
+                        </div>
+                        <div class="td td-price">
+                            <div class="td-wrap">
+                                <div class="td-wrap-price">
+                                    <div class="icon-token eth"></div> 
+                                    <span>12.90 ETH</span>
+                                </div>
+                                <span class="td-light">≈ $ 1000</span>
+                            </div>
+                        </div>
+                        <div class="td"> 
+                            <a class="td-wrap td-wrap-link" href="" target="_blank" rel="nofollow">
+                                <span>a1565...d48d</span>
+                                <i class="i-external-link-line"></i>
+                            </a> 
+                        </div>
+                    </div>
+                    
+                    <div class="table-btn-row">
+                        <button class="btn btn-show-more">
+                            {{translatesGet('SHOW_MORE')}} 
+                            <i class="i-arrow-down-s-line"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+import MultiLang from "@/core/multilang";
+
+export default {
+    data() {
+        return {
+            lang: new MultiLang(this),
+        }
+    },
+    methods:{
+        translatesGet(key) {
+            return this.lang.get(key);
+        },
+    },
+}
+</script>
