82136fb9b9d28a6a993fd2c89ec2d4cedd0d2d51
Elena Pohrebniak <olena@elephantslab.com>
2023-01-10 16:43:00 +0200

    fixed selected img on home

--- src/components/main/animation/images/AnimationImg3.vue
+++ src/components/main/animation/images/AnimationImg3.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <div class="img-container">
-      <img class="svg31" src="./main-animation/31.svg" alt="figure" />
-      <img class="svg32" src="./main-animation/32.svg" alt="figure" />
-      <img class="svg33" src="./main-animation/33.svg" alt="figure" />
-      <img class="svg34" src="./main-animation/34.svg" alt="figure" />
-      <img class="svg35" src="./main-animation/35.svg" alt="figure" />
-      <img class="svg36" src="./main-animation/36.svg" alt="figure" />
-      <img class="svg37" src="./main-animation/37.svg" alt="figure" />
-      <img class="svg38" src="./main-animation/38.svg" alt="figure" />
-      <img class="svg39" src="./main-animation/39.svg" alt="figure" />
-      <img class="svg310" src="./main-animation/310.svg" alt="figure" />
-      <img class="svg311" src="./main-animation/311.svg" alt="figure" />
-      <img class="svg312" src="./main-animation/312.svg" alt="figure" />
+      <img class="svg31" src="./main-animation/31.svg"/>
+      <img class="svg32" src="./main-animation/32.svg"/>
+      <img class="svg33" src="./main-animation/33.svg"/>
+      <img class="svg34" src="./main-animation/34.svg"/>
+      <img class="svg35" src="./main-animation/35.svg"/>
+      <img class="svg36" src="./main-animation/36.svg"/>
+      <img class="svg37" src="./main-animation/37.svg"/>
+      <img class="svg38" src="./main-animation/38.svg"/>
+      <img class="svg39" src="./main-animation/39.svg"/>
+      <img class="svg310" src="./main-animation/310.svg"/>
+      <img class="svg311" src="./main-animation/311.svg"/>
+      <img class="svg312" src="./main-animation/312.svg"/>
     </div>
   </div>
 </template>
