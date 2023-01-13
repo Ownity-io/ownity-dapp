d81b384e631bb5edcae5d27d76b549d0b1e8ed23
Denys Osadchuk <denys.osadchuk@elephantslab.com>
2023-01-13 11:48:47 +0200

    added pie to listing page

--- src/components/listing/chart/Chart.vue
+++ src/components/listing/chart/Chart.vue
@@ -1,77 +1,35 @@
 <template>
-  <DxPieChart id="pie" :data-source="chartData" :palette="customPlate" type="doughnut">
-    <!-- palette="Soft Pastel" -->
-    <DxSeries argument-field="region">
-      <DxLabel :visible="true" format="millions">
-        <DxConnector :visible="true" />
-      </DxLabel>
-    </DxSeries>
-  </DxPieChart>
+<div id="chart">
+  <apexchart id="pie" type="donut" :options="chartOptions" :series="series"></apexchart>
+</div>
 </template>
 
 <script>
-// https://js.devexpress.com/Demos/WidgetsGallery/Demo/Charts/Doughnut/Vue/Light/
-
-import DxPieChart, {
-  // DxLegend,
-  DxSeries,
-  DxTooltip,
-  DxFormat,
-  // DxLabel,
-  DxConnector,
-  DxExport,
-} from "devextreme-vue/pie-chart";
-
-import { customPlate } from "./platet.js";
 
 export default {
-  components: {
-    DxPieChart,
-    // DxLegend,
-    DxSeries,
-    DxTooltip,
-    DxFormat,
-    // DxLabel,
-    DxConnector,
-    DxExport,
-  },
-  data() {
-    return {
-      customPlate,
-    };
-  },
-  props:['chartData']
-  // methods: {
-  //   customizeTooltip({ valueText, percent }) {
-  //     return {
-  //       text: `${valueText} - ${(percent * 100).toFixed(2)}%`,
-  //     };
-  //   },
-  // },
+
+  props:['chartData'],
+  data(){
+    return{
+      series: [44, 55, 41, 17, 15],
+          chartOptions: {
+            chart: {
+              type: 'donut',
+            },
+            responsive: [{
+              breakpoint: 480,
+              options: {
+                chart: {
+                  width: 200
+                },
+                legend: {
+                  position: 'bottom'
+                }
+              }
+            }]
+          },
+    }
+  }
+
 };
 </script>
-<style>
-#pie {
-  /* height: 440px; */
-}
-.dxc-legend,
-.dxc-background,
-.dxc-labels-group ,
-.dxc-annotations,
-.dxc-hole-template,
-
-
-.dxc-legend g,
-.dxc-background g,
-.dxc-labels-group g ,
-.dxc-annotations g,
-.dxc-hole-template g{
-  display: none;
-  width: 0 !important;
-  height: 0 !important;
-  clip-path: none !important;
-}
-.dxc-series-group {
-  width: 100%;
-}
-</style>
