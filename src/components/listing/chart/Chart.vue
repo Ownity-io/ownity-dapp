22b10755894bb2d035e3e27c525062c6470e59d0
Denys Osadchuk <denys.osadchuk@elephantslab.com>
2023-01-13 13:05:25 +0200

    added new diagram to listing page

--- src/components/listing/chart/Chart.vue
+++ src/components/listing/chart/Chart.vue
@@ -1,35 +1,66 @@
 <template>
-<div id="chart">
-  <apexchart id="pie" type="donut" :options="chartOptions" :series="series"></apexchart>
+<div id="chart" v-if="render">
+  <div id="centralPercentage">{{ this.series[this.series.length-1] }}%</div>
+  <apexchart type="donut" :options="chartOptions" :series="series">
+  </apexchart>
 </div>
 </template>
 
 <script>
-
+import colors from './platet.js';
 export default {
-
-  props:['chartData'],
-  data(){
-    return{
-      series: [44, 55, 41, 17, 15],
-          chartOptions: {
-            chart: {
-              type: 'donut',
-            },
-            responsive: [{
-              breakpoint: 480,
-              options: {
-                chart: {
-                  width: 200
-                },
-                legend: {
-                  position: 'bottom'
-                }
-              }
-            }]
-          },
+  props: ['chartData'],
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          type: 'donut',
+        },
+        legend: {
+          show: false
+        },
+        plotOptions:{
+          pie: {
+            startAngle: 90,
+            endAngle:450,
+            donut:{
+              size:'40%'
+            }
+          }
+          
+        },
+        colors: colors
+      },
+      centralPercentage:60,
+      render:false
     }
+  },
+  mounted(){
+    console.log(this.chartData);
+    let tempSeries = [];
+    let tempNames = [];
+    for (let element of this.chartData){
+      tempSeries.push(parseInt(element.val));
+      tempNames.push(element.name);
+    }
+    this.series = tempSeries;
+    this.chartOptions.labels = tempNames;
+    if (this.series.length>1){
+      this.chartOptions.colors[this.series.length-1] = 'rgba(248, 249, 253, 1)';
+    }    
+    console.log(tempNames);
+    this.render = true;
   }
-
 };
 </script>
+
+<style>
+#chart{
+  width: 300px;
+}
+
+/* for customizing label text color */
+.apexcharts-tooltip span {
+    color: #ffffff;
+}
+</style>
