9f85890dba5598ffee8e6000d9688909f8e82e7e
Denys Osadchuk <denys.osadchuk@elephantslab.com>
2023-02-02 11:03:03 +0200

    [NGP-659]Fixed

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -279,15 +279,18 @@
         </div>
       </div>
       <div class="btn-container">
-        <router-link @click="this.$store.dispatch('marketplaceListing/setModalToShowAtStart','appGlobal/setshowStartCollectingModal');" class="btn" :to="'/listing/'+item.collection.contract_address+'/'+item.token_id+'&'+item.id"  v-if="item.marketplace_status=='OPEN' & item.internal_status=='OPEN' & userAddress!=false">
+        <a :href="'/listing/'+item.collection.contract_address+'/'+item.token_id+'&'+item.id" class="btn" v-if="this.$route.name=='Listing'">
+          {{translatesGet('BUY_TOGETHER')}}
+        </a>
+        <router-link @click="this.$store.dispatch('marketplaceListing/setModalToShowAtStart','appGlobal/setshowStartCollectingModal');" class="btn" :to="'/listing/'+item.collection.contract_address+'/'+item.token_id+'&'+item.id"  v-if="item.marketplace_status=='OPEN' & item.internal_status=='OPEN' & userAddress!=false & this.$route.name!='Listing'">
           {{translatesGet('BUY_TOGETHER')}}
         </router-link>
-        <router-link @click="this.$store.dispatch('marketplaceListing/setModalToShowAtStart','appGlobal/setshowContinueCollectingModal');" class="btn" :to="'/listing/'+item.collection.contract_address+'/'+item.token_id+'&'+item.id"  v-if="item.marketplace_status=='OPEN' & item.internal_status=='GATHER'  & userAddress!=false">
-          {{translatesGet('DEPOSIT_PART')}}
-        </router-link>
-        <button class="btn" @click="this.$store.dispatch('appGlobal/setShowConnectWalletModal',true)"  v-if="item.marketplace_status=='OPEN' & item.internal_status=='OPEN' & !userAddress">
+        <button class="btn" @click="this.$store.dispatch('appGlobal/setShowConnectWalletModal',true)"  v-if="item.marketplace_status=='OPEN' & item.internal_status=='OPEN' & !userAddress & this.$route.name!='Listing'">
           {{translatesGet('BUY_TOGETHER')}}
         </button>
+        <router-link @click="this.$store.dispatch('marketplaceListing/setModalToShowAtStart','appGlobal/setshowContinueCollectingModal');" class="btn" :to="'/listing/'+item.collection.contract_address+'/'+item.token_id+'&'+item.id"  v-if="item.marketplace_status=='OPEN' & item.internal_status=='GATHER'  & userAddress!=false">
+          {{translatesGet('DEPOSIT_PART')}}
+        </router-link>        
         <button class="btn"  @click="this.$store.dispatch('appGlobal/setShowConnectWalletModal',true)" v-if="item.marketplace_status=='OPEN' & item.internal_status=='GATHER' & !userAddress">
           {{translatesGet('DEPOSIT_PART')}}
         </button>
